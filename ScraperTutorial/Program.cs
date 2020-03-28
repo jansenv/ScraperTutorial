efb26a347a1ee789786d85af15a8b85bd7e49a61
jansenv <jansenv44@gmail.com>
2020-03-27 21:45:05 -0500

    scraped all h1s and h2s

--- ScraperTutorial/Program.cs
+++ ScraperTutorial/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
 using System.Net.Http;
 
 namespace ScraperTutorial
@@ -7,15 +9,46 @@ namespace ScraperTutorial
     {
         static void Main(string[] args)
         {
+            GetHtmlAsync();
+            Console.ReadLine();
+        }
+
+        // Method for scraping HTML
+        private static async void GetHtmlAsync()
+        {
             // Url you want to scrape from
             var url = "https://jamesnitz.com/";
 
             var httpClient = new HttpClient();
 
             // Stringify & store all html on page
-            var html = httpClient.GetStringAsync(url);
+            var html = await httpClient.GetStringAsync(url);
 
-            Console.WriteLine(html.Result);
+            // Loads html into document so that it can be parsed
+            // From nuget package: HtmlAgilityPack
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // Find all the head tags in the HTML and put them in a list
+            var h1s = htmlDocument.DocumentNode.Descendants("h1").ToList();
+            var h2s = htmlDocument.DocumentNode.Descendants("h2").ToList();
+
+            // Loop through the list and print to console
+
+            Console.WriteLine("----- All <h1> Tags-----");
+            Console.WriteLine("");
+            foreach (var h1 in h1s)
+            {
+                Console.WriteLine($"{h1.InnerHtml}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("----- All <h2> Tags-----");
+            Console.WriteLine("");
+            foreach (var h2 in h2s)
+            {
+                Console.WriteLine($"{h2.InnerHtml}");
+            }
         }
     }
 }
