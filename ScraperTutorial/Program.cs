f706d685a121a15788cd17dc82af09125b27f5a5
jansenv <jansenv44@gmail.com>
2020-03-27 20:59:42 -0500

    all html scraped

---
+++ ScraperTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ScraperTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Url you want to scrape from
+            var url = "https://jamesnitz.com/";
+
+            var httpClient = new HttpClient();
+
+            // Stringify & store all html on page
+            var html = httpClient.GetStringAsync(url);
+
+            Console.WriteLine(html.Result);
+        }
+    }
+}
